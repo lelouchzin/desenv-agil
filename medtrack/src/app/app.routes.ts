8d714548f280364e187d358b3e66eb2c4c0289cf
LuisGustavo2012-dev <luissilva.2024@alunos.utfpr.edu.br>
2026-06-19 11:59:39 -0300

    feat(routing): add lazy-loaded routes for all pages

--- medtrack/src/app/app.routes.ts
+++ medtrack/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./pages/dashboard/dashboard.component').then(m => m.DashboardComponent)
+  },
+  {
+    path: 'remedios',
+    loadComponent: () => import('./pages/remedios/remedios.component').then(m => m.RemediosComponent)
+  },
+  {
+    path: 'notificacoes',
+    loadComponent: () => import('./pages/notificacoes/notificacoes.component').then(m => m.NotificacoesComponent)
+  },
+  {
+    path: 'historico',
+    loadComponent: () => import('./pages/historico/historico.component').then(m => m.HistoricoComponent)
+  },
+];
